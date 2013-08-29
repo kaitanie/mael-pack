8384b555328a0cb584637238345c8a47361e0d7e
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2013-08-29 16:20:19 +0300

    Use the built-in scala-pack insted of referring to external libs

--- init.el
+++ init.el
@@ -6,37 +6,16 @@
 ;; Load bindings config
 (live-load-config-file "bindings.el")
 
-;;(load-file "~/sw/emacs-modes/emacs-scrum/scrum.el")
-(add-to-list 'load-path "~/sw/emacs-modes/scala-mode2/")
+;; Scala-mode2
+(add-to-list 'load-path "~/.live-packs/mael-pack/dev/scala-pack/scala-mode2/")
 (require 'scala-mode2)
 
+
 ;; ENSIME
-;;(add-to-list 'load-path "~/sw/emacs-modes/ensime/ensime_2.10.0-RC3-0.9.8.2/elisp/")
-(add-to-list 'load-path "~/sw/ensime/dist/elisp/")
-(require 'ensime)
-
-(add-to-list 'load-path "~/sw/emacs-modes/emacs-scrum/")
-(require 'scrum)
-
-;; Erlang
-(add-to-list 'load-path "~/sw/distel/elisp")
-(require 'distel)
-(distel-setup)
-
-(defconst distel-shell-keys
-  '(("\C-\M-i"   erl-complete)
-    ("\M-?"      erl-complete)
-    ("\M-."      erl-find-source-under-point)
-    ("\M-,"      erl-find-source-unwind)
-    ("\M-*"      erl-find-source-unwind)
-    )
-  "Additional keys to bind when in Erlang shell.")
-
-(add-hook 'erlang-shell-mode-hook
-          (lambda ()
-            ;; add some Distel bindings to the Erlang shell
-            (dolist (spec distel-shell-keys)
-              (define-key erlang-shell-mode-map (car spec) (cadr spec)))))
-
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/tuareg/")
-
+(let* ((ensime-path "~/.live-packs/mael-pack/dev/scala-pack/ensime/")
+       (ensime-elisp (concat ensime-path "/dist/elisp/"))
+       (ensime-bin (concat ensime-path "/dist/bin/")))
+  (progn
+    (setenv "PATH" (concat ensime-bin ":" (getenv "PATH")))
+    (add-to-list 'load-path ensime-elisp)
+    (require 'ensime)))
